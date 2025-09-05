fdb34a2f003f1bfc676332bdd0810adbadca7230
artemsnoegin <snoegin@gmail.com>
2025-09-05 10:05:36 +0300

    init commit

---
+++ TrafficRacerGame/App/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  TrafficRacerGame
+//
+//  Created by Артём Сноегин on 05.09.2025.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let rootViewController = StartViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
---
+++ TrafficRacerGame/ViewControllers/StartViewController.swift
@@ -0,0 +1,22 @@
+//
+//  StartViewController.swift
+//  TrafficRacerGame
+//
+//  Created by Артём Сноегин on 05.09.2025.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+    }
+    
+}
