7bcc0ae1e2850372f5a4e0c0e4f7fe7f3777adb2
artemsnoegin <snoegin@gmail.com>
2025-09-08 19:00:29 +0300

    add pause button

--- TrafficRacerGame/ViewControllers/MainViewController.swift
+++ TrafficRacerGame/ViewControllers/MainViewController.swift
@@ -13,12 +13,24 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         
         setupUI()
+        addPauseButton()
     }
     
     private func setupUI() {
         let bgView = BackgroundRoadView()
         view.addSubview(bgView)
         
+    }
+    
+    private func addPauseButton() {
+        navigationItem.hidesBackButton = true
+        let pauseBarButton = UIBarButtonItem(image: UIImage(systemName: "pause.fill"), style: .plain, target: self, action: #selector(didPressPauseButton))
+        pauseBarButton.tintColor = .systemYellow
+        navigationItem.rightBarButtonItem = pauseBarButton
+    }
+    
+    @objc private func didPressPauseButton() {
+        navigationController?.popToRootViewController(animated: false)
     }
     
 }
